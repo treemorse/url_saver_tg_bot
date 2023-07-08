5c35a95b9be2f8f1d6674eda8eec6a80220c9539
Mira <mirazondberg@192.168.1.2>
2023-07-08 09:30:44 +0300

    created listner

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use ngrok::prelude::*;
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // build our application with a route
+    let app = Router::new().route("/", get(|| async { "Hello from ngrok-rust!" }));
+
+    // listen on localhost:8000
+    // axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
+    //  .serve(app.into_make_service())
+    //  .await?;
+    // Ok(())
+
+    // listen on ngrok ingress (i.e. https://myapp.ngrok.dev)
+    let listener = ngrok::Session::builder()
+        .authtoken_from_env()
+        .connect()
+        .await?
+        .http_endpoint()
+        .listen()
+        .await?;
+    println!("Ingress URL: {:?}", listener.url());
+    axum::Server::builder(listener)
+        .serve(app.into_make_service())
+        .await?;
+    Ok(())
 }
