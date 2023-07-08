20d00d5eff4dfe02aeee1159930f7a13e814fa30
Mira <mirazondberg@192.168.1.2>
2023-07-08 09:41:27 +0300

    created sample ngrok-rust web app

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,8 @@ use std::error::Error;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    // build our application with a route
     let app = Router::new().route("/", get(|| async { "Hello from ngrok-rust!" }));
 
-    // listen on localhost:8000
-    // axum::Server::bind(&"0.0.0.0:8000".parse().unwrap())
-    //  .serve(app.into_make_service())
-    //  .await?;
-    // Ok(())
-
-    // listen on ngrok ingress (i.e. https://myapp.ngrok.dev)
     let listener = ngrok::Session::builder()
         .authtoken_from_env()
         .connect()
